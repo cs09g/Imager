83cc1bb2ae46ec8802809b773aa014de5b56a424
cs09g <cs09gi@gmail.com>
2016-10-16 16:06:58 +0900

    added compressing command using uglifyjs2

--- server.js
+++ server.js
@@ -4,12 +4,27 @@ var fs = require('fs');
 var path = require('path');
 var ejs = require('ejs-locals');
 var bodyParser = require('body-parser');
+var uglifyJS = require('uglify-js');
+
+var result = uglifyJS.minify(path.join(__dirname, '/static/js/imgrMain.js'), {
+	mangleProperties: {
+		regex: /^_/
+	}
+});
+
+fs.writeFile(path.join(__dirname, '/release/imgrMain.min.js'), result.code, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    console.log('minified code is created');
+}); 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use('/Bootstrap', express.static(path.join(__dirname, '/static/external/Bootstrap')));
 app.use('/extern/js', express.static(path.join(__dirname, '/static/external/js')));
 app.use('/js', express.static(path.join(__dirname, '/static/js')));
+app.use('/release', express.static(path.join(__dirname, '/release')));
 
 app.engine('ejs', ejs);
 app.set('view engine', 'ejs');
